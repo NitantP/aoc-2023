e0aacdb150bbf6cf77c1e27a1076ed16207108c2
Nitant Patel <patel.nitant.bca@gmail.com>
2023-12-10 10:21:41 -0500

    Remove inputs from git tree

---
+++ src/day_10/solution.rs
@@ -0,0 +1,17 @@
+// https://adventofcode.com/2023/day/CHANGEME
+
+fn solve_part_1(input: &str) -> u64 {
+    todo!();
+}
+
+fn solve_part_2(input: &str) -> u64 {
+    todo!();
+}
+
+pub fn answers() {
+    // let input = include_str!("input.txt").trim();
+    let input = include_str!("test.txt").trim();
+
+    println!("[P1 :: INFO] Answer: {}", solve_part_1(input));
+    println!("[P2 :: INFO] Answer: {}", solve_part_2(input));
+}
